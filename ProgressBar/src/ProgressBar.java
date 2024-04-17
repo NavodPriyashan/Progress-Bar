815b448ac9f147935e188fadfbebef5e9ad72aa9
NavodPriyashan <120322257+NavodPriyashan@users.noreply.github.com>
2024-04-17 18:39:51 +0530

    Progress Bar

---
+++ ProgressBar/src/ProgressBar.java
@@ -0,0 +1,12 @@
+
+public class ProgressBar {
+
+	public static void main(String[] args) {
+		
+		//progress bar = Visual aid to let the user know that an operation is progress
+		
+		ProgressBarDemo demo = new ProgressBarDemo();
+
+	}
+
+}
